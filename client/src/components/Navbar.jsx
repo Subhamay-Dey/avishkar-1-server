4e1ffc73e27760019d5a17ed9442a2adb6161f89
Subhamay-Dey <ncdey1966@gmail.com>
2025-04-08 12:44:33 +0530

    feat: replace School icon with logo image in Navbar and add Menu button in MobileNavbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -73,7 +73,7 @@ const Navbar = () => {
       {/* Desktop */}
       <div className="max-w-7xl mx-auto hidden md:flex justify-between items-center gap-10 h-full">
         <div className="flex items-center gap-2">
-          <School size={"30"} />
+          <image src="/WebsiteLogo.png" alt="logo" className="w-10 h-10" />
           <Link to="/">
             <h1 className="hidden md:block font-extrabold text-2xl">
               E-Learning
@@ -207,7 +207,13 @@ const MobileNavbar = ({user, pendingCount, countFetched}) => {
   return (
     <Sheet>
       <SheetTrigger asChild>
-
+        <Button
+          size="icon"
+          className="rounded-full hover:bg-gray-200"
+          variant="outline"
+        >
+          <Menu />
+        </Button>
       </SheetTrigger>
       <SheetContent className="flex flex-col">
         <SheetHeader className="flex flex-row items-center justify-between mt-2">
