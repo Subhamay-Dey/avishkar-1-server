2ec976c127cffb6a9623f46e0e59e3cf8fbc5e96
Subhamay-Dey <ncdey1966@gmail.com>
2025-04-08 14:04:35 +0530

    Refactor code structure for improved readability and maintainability

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -73,7 +73,7 @@ const Navbar = () => {
       {/* Desktop */}
       <div className="max-w-7xl mx-auto hidden md:flex justify-between items-center gap-10 h-full">
         <div className="flex items-center gap-2">
-          <img src="/AvishkarLogo.png" alt="logo" className="w-20 h-8" />
+          <img src="/AvishkarLogo.jpg" alt="logo" className="w-20 h-8" />
           <Link to="/">
             <h1 className="hidden md:block font-extrabold text-2xl">
               E-Learning
